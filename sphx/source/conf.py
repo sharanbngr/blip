7765620253b26c2d7300f157af60e1603b46e63b
Sharan Banagiri <banag002@umn.edu>
2019-03-13 23:21:28 -0700

    Restructure docs

--- sphx/source/conf.py
+++ sphx/source/conf.py
@@ -12,10 +12,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
-sys.setrecursionlimit(1500)
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -26,7 +26,7 @@ author = u'Sharan Banagiri'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.01'
 
 
 # -- General configuration ---------------------------------------------------
@@ -40,6 +40,7 @@ release = u'0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
@@ -71,7 +72,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
--- sphx/source/index.rst
+++ sphx/source/index.rst
@@ -1,10 +1,8 @@
 .. blip documentation master file, created by
-   sphinx-quickstart on Wed Mar 13 22:40:09 2019.
+   sphinx-quickstart on Wed Mar 13 23:19:19 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Blip is a pythonic bayesian pipeline for doing stochastic analysis using LISA.
-   
 Welcome to blip's documentation!
 ================================
 
