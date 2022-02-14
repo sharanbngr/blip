8cbfeeeeaa026545d1e29df46266ba3804f52126
Alexander Criswell <39200222+criswellalexander@users.noreply.github.com>
2022-02-14 12:38:04 -0600

    Add new population fx
    - Added new function to generate skymaps from pop synth datasets (not yet connected to anything)
    - updated simulated DWD FG toy model with faster map generation

--- src/makeLISAdata.py
+++ src/makeLISAdata.py
@@ -693,6 +693,82 @@ class LISAdata(geometry, sph_geometry, instrNoise):
 
         return h1, h2, h3, times
 
+# This function should be incorporated in future, but I need to think a little carefully about how to integrate it first.
+#    def pop2spec(fs,hs,t_obs,fmin=5e-5,fmax=1e-1,output='foreground'):
+#        '''
+#        Function to calculate the foreground spectrum arising from a population catalogue of unresolved DWD binaries.
+#        
+#        Arguments:
+#            fs (1D array of floats) : Binary frequencies. Assumed monochromatic.
+#            hs (1D array of floats) : Binary strains.
+#            t_obs (astropy Quantity, time units) : Observation time.
+#            plot (bool) : Whether to also produce a plot of the foreground spectrum. Default True.
+#            saveto (str) : If specified, the location to which the plot will be saved. Default None (won't save).
+#            fmin, fmax (float) : Minimum and maximum frequencies of the calculated and plotted PSD.
+#            output (str) : Can be 'foreground' or 'all'. Which sets of PSDs to return.
+#                           If 'foreground', only the foreground PSD and frequencies will be returned.
+#                           If 'all', total PSD and detector PSD will also be returned.
+#        
+#        Returns:
+#            (if output=='foreground'):
+#            fg_PSD_fullspec (array of floats) : Resulting PSD of unresolved binary background/foreground
+#            fs_all (array of floats) : Frequencies at which PSD is evaluated.
+#            (additionally, if output=='all'):
+#            det_PSD (array of floats) : Detector PSD evaluated at fs_all
+#            PSD_tot (array of floats) : Total PSD (detector noise + foreground)
+#        '''
+#        ## set bin to delta_f = 1/T_obs
+#        bin_width = (1/t_obs).to(u.Hz)
+#        ## get unresolved system PSDs
+#        PSDs_unres = t_obs*hs**2
+#        ## bin and sum
+#        edges = np.arange(fs_unres.min().value,fs_unres.max().value+bin_width.value,bin_width.value)
+#        fg_PSD, bins = np.histogram(fs_unres.value,bins=edges,weights=PSDs_unres)
+#        mids = bins[:-1]+bin_width.value
+#        ## get total PSD
+#        fs_low = np.arange(fmin,mids[0]-bin_width.value,bin_width.value)
+#        fs_high = np.arange(mids[-1]+bin_width.value,fmax,bin_width.value)
+#        fs_all = np.append(np.append(fs_low,mids),fs_high)
+#        PSD_tot = np.append(np.append(lw.psd.lisa_psd(fs_low*u.Hz,t_obs=t_obs,confusion_noise=None),
+#                                      lw.psd.lisa_psd(mids*u.Hz,t_obs=t_obs,confusion_noise=None)+fg_PSD),
+#                            lw.psd.lisa_psd(fs_high*u.Hz,t_obs=t_obs,confusion_noise=None))
+#        ## extend foreground PSD to match with fs_all (filling in zeros where there is no foreground contribution)
+#        fg_PSD_fullspec = np.append(np.append(np.zeros(len(fs_low)),fg_PSD.value),np.zeros(len(fs_high)))/u.Hz
+#        ## get detector PSD
+#        det_PSD = lw.psd.lisa_psd(fs_all*u.Hz,t_obs=t_obs,confusion_noise=None)
+#        
+#        if output=='foreground':
+#            return fg_PSD_fullspec, fs_all
+#        elif output=='all':
+#            return fg_PSD_fullspec, fs_all, det_PSD, PSD_tot
+#        else:
+#            print("Invalid specification of output. Can be 'foreground' or 'all'.")
+    
+    def skymap_from_pop(self,lats,longs,PSDs,nside):
+        '''
+        Function to get a skymap from a catalogue of binaries. 
+        Note that this function will process all binaries given to it; SNR filtering must be done beforehand.
+        
+        Arguments:
+            lats, longs (arrays of floats) : Latitudes and longitudes of catalogue binaries. 
+                                             IMPORTANT: Must be given in ecliptic coordinates and units of degrees!
+            PSDs (array of floats) : Corresponding catalogue binary PSDs (assumed monochromatic)
+            nside (int) : Healpix nside to use for skymap. Must be power of 2 < 2**32.
+            
+        Returns:
+            skymap (array of floats) : Healpix skymap of GW power on the sky
+            logskymap (array of floats) : Healpix skymap of log GW power on the sky
+        '''
+        ## convert sky coordinates to healpy pixels
+        pix_idx = hp.ang2pix(nside,longs,lats,lonlat=True)
+        ## sum power from all binaries in same pixel
+        skymap = np.bincount(pix_idx,weights=PSDs.value,minlength=hp.nside2npix(nside))
+        ## set any zero pixels to a very small number to avoid problems with taking the log
+        skymap[skymap<=0] = 1e-80
+        ## get log
+        logskymap = np.log10(skymap)
+        return skymap, logskymap
+    
     def generate_galactic_foreground(self, rh=2.9, zh=0.3):
         '''
         Generate a galactic white dwarf binary foreground modeled after Breivik et al. (2020), consisting of a bulge + disk.
@@ -743,8 +819,10 @@ class LISAdata(geometry, sph_geometry, instrNoise):
         ## Create skymap
         DWD_FG_mapG = np.zeros(hp.nside2npix(2*self.params['nside']))
         ## Bin
-        for i in range(DWD_FG_mapG.size):
-            DWD_FG_mapG[i] = np.sum((pixels==i)*DWD_unresolved_powers)
+#        for i in range(DWD_FG_mapG.size):
+#            DWD_FG_mapG[i] = np.sum((pixels==i)*DWD_unresolved_powers)
+        ## updated faster binning
+        DWD_FG_mapG = np.bincount(pixels,weights=DWD_unresolved_powers,minlength=hp.nside2npix(2*self.params['nside']))
         ## create logarithmic skymap for plotting purposes
         log_DWD_FG_mapG = np.log10(DWD_FG_mapG + 10**-15 * (DWD_FG_mapG==0))
         ## Transform into the ecliptic
