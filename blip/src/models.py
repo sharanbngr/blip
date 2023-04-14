dd96e4cba22e0b37d2b8e05b23c6f5aa5b98be4f
Arianna Renzini <arianna.renzini@ligo.org>
2023-04-14 10:04:25 -0700

    fixing imports, adding dependencies

--- blip/src/models.py
+++ blip/src/models.py
@@ -2,13 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.interpolate import interp1d
 import healpy as hp
-from src.geometry import geometry
-from src.sph_geometry import sph_geometry
-from src.clebschGordan import clebschGordan
-from src.astro import Population
-from src.likelihoods import likelihoods
-from src.instrNoise import instrNoise
-import src.astro as astro
+from blip.src.geometry import geometry
+from blip.src.sph_geometry import sph_geometry
+from blip.src.clebschGordan import clebschGordan
+from blip.src.astro import Population
+from blip.src.likelihoods import likelihoods
+from blip.src.instrNoise import instrNoise
+import blip.src.astro as astro
 
 
 
