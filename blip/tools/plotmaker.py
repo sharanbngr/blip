ef177a1ce6f35041e607fe6c6282db64cd2df866
Alexander Criswell <39200222+criswellalexander@users.noreply.github.com>
2023-05-06 02:23:28 -0500

    Update plotmaker.py
    + plot formatting fix
    + proper plot label aliasing

--- blip/tools/plotmaker.py
+++ blip/tools/plotmaker.py
@@ -276,6 +276,7 @@ def fitmaker(post,params,parameters,inj,Model,Injection=None,saveto=None,plot_co
     ymins = []
     ## loop over submodels
     signal_model_names = [sm_name for sm_name in Model.submodel_names if sm_name!='noise']
+    signal_aliases = [Model.submodels[sm_name].alias for sm_name in signal_model_names if hasattr(Model.submodels[sm_name],"alias")]
     for i, sm_name in enumerate(signal_model_names):
         sm = Model.submodels[sm_name]
         model_legend_elements.append(Line2D([0],[0],color=sm.color,lw=3,label=sm.fancyname))
@@ -310,7 +311,7 @@ def fitmaker(post,params,parameters,inj,Model,Injection=None,saveto=None,plot_co
                 if component_name in astro_kwargs['color_dict'].keys():
                     kwargs['color'] = astro_kwargs['color_dict'][component_name]
                 Injection.plot_injected_spectra(component_name,fs_new=fs,legend=False,ymins=ymins,**kwargs)
-                if component_name not in Model.submodel_names:
+                if component_name not in Model.submodel_names and component_name not in signal_aliases:
                     model_legend_elements.append(Line2D([0],[0],color=Injection.components[component_name].color,lw=3,label=Injection.components[component_name].fancyname))
     
     ## avoid plot squishing due to signal spectra with cutoffs, etc.
@@ -406,7 +407,7 @@ def fitmaker(post,params,parameters,inj,Model,Injection=None,saveto=None,plot_co
                     Injection.plot_injected_spectra(component_name,channels='22',ymins=ymins,**kwargs)
                 else:
                     Injection.plot_injected_spectra(component_name,fs_new=fdata,convolved=True,ymins=ymins,**kwargs)
-                    if component_name not in Model.submodel_names:
+                    if component_name not in Model.submodel_names and component_name not in signal_aliases:
                         model_legend_elements.append(Line2D([0],[0],color=Injection.components[component_name].color,lw=3,label=Injection.components[component_name].fancyname))
         
         ## avoid plot squishing due to signal spectra with cutoffs, etc.
@@ -586,6 +587,8 @@ if __name__ == '__main__':
     
     
     post = np.loadtxt(params['out_dir'] + "/post_samples.txt")
+    
+    matplotlib.rcParams.update(matplotlib.rcParamsDefault)
     
     if not args.nocorner:
         plotmaker(post, params, parameters, inj, Model, Injection)    
