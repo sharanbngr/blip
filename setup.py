ec8871c5b99eeecde627cb809959d9aca1651314
sharanbngr <sharanbngr@gmail.com>
2023-06-20 22:27:26 -0500

    updating install info and python version requirement.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires="==3.10",
+    python_requires=">=3.10",
 )
 
 
